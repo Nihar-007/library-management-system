da4204a391c5b3e3e6b2cf45cd461211238aad61
Nihar <nihardankhara1@gmail.com>
2023-10-09 15:59:35 +0530

    added userpage.php

--- signup.php
+++ signup.php
@@ -23,8 +23,10 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
         if ($pass == $cpass) {
             $sql = "INSERT INTO `login` (`fname`,`lname`,`uname`,`pwd`) values ('$fname','$lname','$uname','$pass')";
             $result = mysqli_query($conn, $sql);
-            echo '<script> alert("Account Created."); </script>';
-            header("location: login.php");
+            if($result){
+              header("location: login.php");
+            }
+            // echo '<script> alert("Account Created."); </script>';
         } else {
             echo '<script> alert("Passwords dont match please try again."); </script>';
         }
@@ -32,7 +34,6 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 }
 
 ?>
-
 <!DOCTYPE html>
 <html lang="en">
 
@@ -40,12 +41,187 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <!-- linking CSS File -->
-    <link rel="stylesheet" href="login.css">
+    <!-- <link rel="stylesheet" href="login.css"> -->
     <title>Sign-Up</title>
 </head>
 
+<style>
+     html {
+                height: 100%;
+              }
+              body {
+                margin:0;
+                padding:0;
+                font-family: sans-serif;
+                background:url("4.jpg") no-repeat ;
+                background-size: cover;
+                background-position: center;
+              }
+              
+              .login-box {
+                position: absolute;
+                top: 50%;
+                left: 50%;
+                width: 550px;
+                padding: 40px;
+                transform: translate(-50%, -50%);
+                background: rgba(0,0,0,.5);
+                box-sizing: border-box;
+                box-shadow: 0 15px 25px rgba(0,0,0,.6);
+                border-radius: 10px;
+              }
+              
+              .login-box h2 {
+                margin: 0 0 30px;
+                padding: 0;
+                color: #fff;
+                text-align: center;
+              }
+              
+              .login-box .user-box {
+                position: relative;
+              }
+              
+              .login-box .user-box input {
+                width: 100%;
+                padding: 10px 0;
+                font-size: 16px;
+                color: #fff;
+                margin-bottom: 30px;
+                border: none;
+                border-bottom: 1px solid #fff;
+                outline: none;
+                background: transparent;
+              }
+              .login-box .user-box label {
+                position: absolute;
+                top:0;
+                left: 0;
+                padding: 10px 0;
+                font-size: 16px;
+                color: #fff;
+                pointer-events: none;
+                transition: .5s;
+              }
+              
+              .login-box .user-box input:focus ~ label,
+              .login-box .user-box input:valid ~ label {
+                top: -20px;
+                left: 0;
+                color: #03e9f4;
+                font-size: 12px;
+              }
+              
+              .login-box form .a {
+                position: relative;
+                display: inline-block;
+                padding: 10px 20px;
+                color: #03e9f4;
+                font-size: 16px;
+                text-decoration: none;
+                text-transform: uppercase;
+                background: transparent;
+                border: 0px;
+                overflow: hidden;
+                transition: .5s;
+                margin-top: 15px;
+                letter-spacing: 4px
+              }
+              
+              .login-box .a:hover {
+                background: #03e9f4;
+                color: #fff;
+                border-radius: 5px;
+                box-shadow: 0 0 5px #03e9f4,
+                            0 0 25px #03e9f4,
+                            0 0 50px #03e9f4,
+                            0 0 100px #03e9f4;
+              }
+              
+              .login-box .a span {
+                position: absolute;
+                display: block;
+              }
+              
+              .login-box .a span:nth-child(1) {
+                top: 0;
+                left: -100%;
+                width: 100%;
+                height: 2px;
+                background: linear-gradient(90deg, transparent, #03e9f4);
+                animation: btn-anim1 1s linear infinite;
+              }
+              
+              @keyframes btn-anim1 {
+                0% {
+                  left: -100%;
+                }
+                50%,100% {
+                  left: 100%;
+                }
+              }
+              
+              .login-box .a span:nth-child(2) {
+                top: -100%;
+                right: 0;
+                width: 2px;
+                height: 100%;
+                background: linear-gradient(180deg, transparent, #03e9f4);
+                animation: btn-anim2 1s linear infinite;
+                animation-delay: .25s
+              }
+              
+              @keyframes btn-anim2 {
+                0% {
+                  top: -100%;
+                }
+                50%,100% {
+                  top: 100%;
+                }
+              }
+              
+              .login-box .a span:nth-child(3) {
+                bottom: 0;
+                right: -100%;
+                width: 100%;
+                height: 2px;
+                background: linear-gradient(270deg, transparent, #03e9f4);
+                animation: btn-anim3 1s linear infinite;
+                animation-delay: .5s
+              }
+              
+              @keyframes btn-anim3 {
+                0% {
+                  right: -100%;
+                }
+                50%,100% {
+                  right: 100%;
+                }
+              }
+              
+              .login-box .a span:nth-child(4) {
+                bottom: -100%;
+                left: 0;
+                width: 2px;
+                height: 100%;
+                background: linear-gradient(360deg, transparent, #03e9f4);
+                animation: btn-anim4 1s linear infinite;
+                animation-delay: .75s
+              }
+              
+              @keyframes btn-anim4 {
+                0% {
+                  bottom: -100%;
+                }
+                50%,100% {
+                  bottom: 100%;
+                }
+              }
+              
+</style>
+
 <body>
-    <header>
+    <!-- <header>
         <p>Welcome to Sign-up page</p>
     </header>
     <main>
@@ -69,9 +245,61 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
             <p>Already have an account with us ? <a href="login.php">Click Here!</a></p>
         </form>
     </main>
-    <footer>
-        <p>&copy; Copywrite 2023-2023, SOU. All rights are reserved.</p>
-    </footer>
-</body>
+     -->
+     <div class="login-box">
+            <h2>Welcome to Sign-up page</h2>
+            <form method="post">
+              <div class="user-box">
+              
+            <input type="text" name="fname" id="fname" required>
+            <label for="fname">First name</label>
 
+              </div>
+              <div class="user-box">
+              
+            <input type="text" name="lname" id="lname" required>
+            <label for="lname">Last name</label>
+            
+              </div>
+              
+              <div class="user-box">
+             
+            <input type="text" name="uname" id="uname" required>
+            <label for="uname">Username</label>
+            
+              </div>
+
+
+
+              <div class="user-box">
+              
+            <input type="password" name="pass" id="pass" minlength="8" required>
+            <label for="pass">Password</label>
+              </div>
+
+
+              <div class="user-box">
+             
+            <input type="password" name="cpass" id="cpass" minlength="8" required>
+            <label for="cpass">Confirm Password</label>
+              </div>
+
+
+
+              <!-- <a href="#">
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>
+                Submit
+              </a> -->
+              <button class="a" type="submit" name="submit">
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>Sign-Up</button>
+              <p style="color:#ffff; margin-left: 48px;margin-top:20px;">Already have an account with us ? <a href="login.php" class="a" style="margin: top 10px;margin-top: 10px;margin-left: 30px; ">Click Here!</a></p>
+            </form>
+          </div>
+</body>
 </html>
--- userpage.php
+++ userpage.php
@@ -18,12 +18,13 @@ if ($_SESSION['login'] != true) {
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-  <link rel="stylesheet" href="addmin.css">
+  <link rel="stylesheet" href="userpage.css">
   <title>Library management system</title>
 
 </head>
 
 <body>
+  <div class="bg"></div>
   <header>
     <div class="mynavbar">
 
@@ -45,54 +46,63 @@ if ($_SESSION['login'] != true) {
           <li><a href="#">CONTACT</a></li>
           <li><a href="#">REQUEST'S</a></li>
           <li><a href="logout.php">LOGOUT</a></li>
+          <!-- <li><select name="PURCHASE" id="Others">
+                <option value="#"><a href="#">PURCHASE</a></option>
+                <option value="#"><a href="#">BORROW</a></option>
+                <option value="psychology"><a href="#">RETURN</a></option>
+                <option value="Noval"><a href="#">RENEW</a></option>
+                <option value="Motavation"><a href="#">ADD BOOKS</a></option>
+                <option value="History"><a href="#">REMOVE BOOKS</a></option>
+              </select></li> -->
+
         </ul>
       </div>
 
       <div class="nav-right">
-        <!-- <div> -->
-          <form action="#" method="post" class="search-bar">
-            <input type="text" name="search-query" placeholder="Search" />
-            <button type="submit" class="search-btn">
-              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 40 40" stroke-width="2" stroke="black" class="w-4 h-4" color="black">
-                <path stroke-linecap="round" stroke-linejoin="round" d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z" />
-              </svg>
-            </button>
-          </form>
-          <div class="nav-icon">
-            <button style="height: 18px; border: 0px;"><i class="fa-solid fa-bars fa-2xl"></i></button>
-          </div>
-        <!-- </div> -->
+
+        <form action="#" method="post" class="search-bar">
+          <input type="text" name="search-query" placeholder="Search" />
+          <button type="submit" class="search-btn">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 40 40" stroke-width="2" stroke="black" class="w-4 h-4" color="black">
+              <path stroke-linecap="round" stroke-linejoin="round" d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z" />
+            </svg>
+          </button>
+        </form>
       </div>
 
     </div>
 
   </header>
+
+
   <aside>
     <div class="wrapper">
 
 
       <div class="sidebar">
         <div class="logo2">
-          <img src="libaray_logo1.png" alt="logo" class="default">
-          <h2>library</h2>
+          <img src="libaray_logo1.png" alt="logo">
+          <h2>libarary</h2>
         </div>
         <ul class="links">
-          <h4>Main Menu</h4>
-          <li><a href="#">ISSUE</a></li>
-          <!-- <li><a href="#">RENEW</a></li> -->
-          <li><a href="#">RETURN</a></li>
-          <li><a href="add_book.php">ADD BOOKS</a></li>
-          <li><a href="remove_book.php">REMOVE BOOKS</a></li>
-          <li><a href="admin.php">USER DATABASE</a></li>
+          <h4>Category of book's</h4>
+          <li><a href="#">SCIENCE</a></li>
+          <li><a href="#">IT Computer</a></li>
+          <li><a href="#">Maths</a></li>
+          <li><a href="#">Commers</a></li>
+          <li><a href="#">Arts</a></li>
+          <li><a href="#">Letrecher</a></li>
           <hr>
 
         </ul>
+
       </div>
 
       <!-- books          coding      start -->
 
+
       <div class="info">
-        <!-- First book self -->
+
         <div id="slider-container">
           <h1>MOTIVATION</h1>
           <button id="prev-btn" onclick="prevSlide()"> &lt; </button>
@@ -109,7 +119,7 @@ if ($_SESSION['login'] != true) {
                 $bname = $row['b_name'];
                 $bimg = $row['b_image'];
                 echo "<div class='slider-item'><a href=''><img src='images/$bimg' alt='$bname'></a>
-                      <p>".$row['b_name']."</p></div>";
+                      <p>" . $row['b_name'] . "</p></div>";
                 // echo "<div>".$row['b_name']."</div>";
               }
               ?>
@@ -135,7 +145,7 @@ if ($_SESSION['login'] != true) {
                 $bname = $row['b_name'];
                 $bimg = $row['b_image'];
                 echo "<div class='slider-item'><a href=''><img src='images/$bimg' alt='$bname'></a>
-                <p>".$row['b_name']."</p></div>";
+                <p>" . $row['b_name'] . "</p></div>";
               }
               ?>
             </div>
@@ -159,7 +169,7 @@ if ($_SESSION['login'] != true) {
                 $bname = $row['b_name'];
                 $bimg = $row['b_image'];
                 echo "<div class='slider-item'><a href=''><img src='images/$bimg' alt='$bname'></a>
-                <p>".$row['b_name']."</p></div>";
+                <p>" . $row['b_name'] . "</p></div>";
               }
               ?>
             </div>
@@ -184,7 +194,7 @@ if ($_SESSION['login'] != true) {
                 $bname = $row['b_name'];
                 $bimg = $row['b_image'];
                 echo "<div class='slider-item'><a href=''><img src='images/$bimg' alt='$bname'></a>
-                <p>".$row['b_name']."</p></div>";
+                <p>" . $row['b_name'] . "</p></div>";
               }
               ?>
             </div>
@@ -210,7 +220,7 @@ if ($_SESSION['login'] != true) {
                 $bname = $row['b_name'];
                 $bimg = $row['b_image'];
                 echo "<div class='slider-item'><a href=''><img src='images/$bimg' alt='$bname'></a>
-                <p>".$row['b_name']."</p></div>";
+                <p>" . $row['b_name'] . "</p></div>";
               }
               ?>
             </div>
@@ -236,7 +246,7 @@ if ($_SESSION['login'] != true) {
                 $bname = $row['b_name'];
                 $bimg = $row['b_image'];
                 echo "<div class='slider-item'><a href=''><img src='images/$bimg' alt='$bname'></a>
-                <p>".$row['b_name']."</p></div>";
+                <p>" . $row['b_name'] . "</p></div>";
               }
               ?>
             </div>
@@ -279,7 +289,15 @@ if ($_SESSION['login'] != true) {
 
       </div>
     </div>
+
   </aside>
+
+
+
+
+
+
+
 
 </body>
 
