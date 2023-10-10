b403662dd33013466dfe350a8a024f22958198f8
Nihar <nihardankhara1@gmail.com>
2023-10-10 19:44:23 +0530

    Added css in book_open.php

--- book_open.php
+++ book_open.php
@@ -8,61 +8,33 @@
     // while($row = mysqli_fetch_assoc($result))
 
 ?>
-<style>
-    *{
-        margin: 0;
-        padding: 0;
-    }
-    header {
-        background-color: black;
-        width: 100%;
-        height: 5vh;
-        display: flex;
-        justify-content: center;
-        align-items: center;
-    }
-    header > a{
-        color: white;
-        text-decoration: none;
-    }
-    main{
-        height: 90vh;
-    }
-    footer{
-        display: flex;
-        justify-content: center;
-        align-items: center;
-        height: 5vh;
-        background-color: black;
-        color: white;
-    }
-</style>
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title> <?php echo $bname; ?> </title>
+    <link rel="stylesheet" href="css/book_open.css">
 </head>
 <body>
     <header>
-        <a href="userpage.php">HOME</a>
+        <p><a href="userpage.php">HOME</a></p>
     </header>
     <main>
         <form method="post">
-            <div>
-                <p>Book Image : <img src="images/<?php echo $row['b_image']; ?>" alt="<?php echo $row['b_name']; ?>" width="150px"></p>
-                <p>Book Name : <?php echo $bname; ?></p>
-                <p>Author Name : <?php echo $row['b_author']; ?></p>
-                <p>Publisher : <?php echo $row['b_publisher']; ?></p>
-                <p>Number Of Pages : <?php echo $row['b_page']; ?></p>
-                <p><a href="">Click Here!</a> To Download This Book</p>
+         <img src="images/<?php echo $row['b_image']; ?>" alt="<?php echo $row['b_name']; ?>" width="150px">
+            <div class="contant">
+                
+                <p><label for="">Book Name</label><?php echo $bname; ?></p>
+                <p><label for="">Author Name</label><?php echo $row['b_author']; ?></p>
+                <p><label for="">Publisher</label><?php echo $row['b_publisher']; ?></p>
+                <p><label for="">Number Of Pages</label><?php echo $row['b_page']; ?></p>
+                <p><a href="" Download>Click Here!</a> To Download This Book</p>
             </div>
         </form>
     </main>
     <footer>
-        <p>&copy;Library 2023-2023, All Rights Are Reserved.</p>
+        <p id="copy">&copy;Library 2023-2023, All Rights Are Reserved.</p>
     </footer>
 </body>
 </html>
